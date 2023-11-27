f1ae4248d8202d1c1861d7bb915104ece9e053c7
a-0moon <151838350+a-0moon@users.noreply.github.com>
2023-11-27 20:00:03 +0430

    Add files via upload
     It is a fashion focus project that done by React js.

---
+++ App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+import Header from './components/Header.jsx';
+import Shop from './components/Shop.jsx';
+import { DUMMY_PRODUCTS } from './dummy-products.js';
+import Product from './components/Product.jsx';
+import ContextCartProvider, { ContextCart } from './store/shopping-cart-context.jsx';
+
+function App() {
+
+
+  return (
+    <ContextCartProvider>
+
+      <Header />
+      <Shop >
+
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} />
+          </li>
+        ))}
+
+      </Shop>
+    </ContextCartProvider>
+
+  );
+}
+
+export default App;
